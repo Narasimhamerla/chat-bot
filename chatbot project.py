169ac55829424bfc8611b6378b7db7744f12a50c
Narasimha <126140475+Narasimhamerla@users.noreply.github.com>
2025-09-22 17:12:39 +0530

    Add files via upload

---
+++ chatbot project.py
@@ -0,0 +1,10 @@
+import pandas as pd 
+import numpy as np
+import matplotlib.plyplot as plt 
+%matplotlib inline
+x1 = np.linspace(0,10,100)
+fig = plt.figure()
+plt.plot(x1,np.sin(x1),'-')
+plt.plot(x1,np.cos(x1),'--');
+pip show matplotlib
+pip install matplotlib
